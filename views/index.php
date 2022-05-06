7aa4cc2c3c6253cba06f679c5f94424a3cc1cbcf
Martin Mbithi <martdevelopers254@gmail.com>
2022-05-06 08:18:02 +0300

    Implement Index Pages - Show List Of Users

--- views/index.php
+++ views/index.php
@@ -189,89 +189,32 @@ require_once('../partials/head.php');
             <div class="container">
                 <div class="section-header">
                     <h4 class="theme-color">Meet New People Today!</h4>
-                    <h2>New Members in London</h2>
+                    <h2>Members Near You</h2>
                 </div>
                 <div class="section-wrapper">
                     <div class="row justify-content-center g-3 g-md-4">
-                        <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-                            <div class="lab-item member-item style-1">
-                                <div class="lab-inner">
-                                    <div class="lab-thumb">
-                                        <img src="../public/images/member/01.jpg" alt="member-img">
-                                    </div>
-                                    <div class="lab-content">
-                                        <h6><a href="profile.html">Andrea Guido</a> </h6>
-                                        <p>Active 1 Day</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-                            <div class="lab-item member-item style-1">
-                                <div class="lab-inner">
-                                    <div class="lab-thumb">
-                                        <img src="../public/images/member/02.jpg" alt="member-img">
-                                    </div>
-                                    <div class="lab-content">
-                                        <h6><a href="profile.html">Gihan-Fernando</a></h6>
-                                        <p>Active 2 Day</p>
+                        <?php
+                        $ret = "SELECT * FROM  users WHERE user_account_status = 'Verified'";
+                        $stmt = $mysqli->prepare($ret);
+                        $stmt->execute(); //ok
+                        $res = $stmt->get_result();
+                        while ($users = $res->fetch_object()) {
+                        ?>
+                            <div class="col-xl-2 col-lg-3 col-md-4 col-6">
+                                <div class="lab-item member-item style-1">
+                                    <div class="lab-inner">
+                                        <div class="lab-thumb">
+                                            <img src="../public/uploads/user_data/<?php echo $users->user_profile_picture; ?>" alt="member-img">
+                                        </div>
+                                        <div class="lab-content">
+                                            <h6><a href="login"><?php echo $users->user_name; ?></a> </h6>
+                                            <p><?php echo $users->user_status; ?></p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-                            <div class="lab-item member-item style-1">
-                                <div class="lab-inner">
-                                    <div class="lab-thumb">
-                                        <img src="../public/images/member/03.jpg" alt="member-img">
-                                    </div>
-                                    <div class="lab-content">
-                                        <h6><a href="profile.html">Sweet Admin</a></h6>
-                                        <p>Active 3 Day</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-                            <div class="lab-item member-item style-1">
-                                <div class="lab-inner">
-                                    <div class="lab-thumb">
-                                        <img src="../public/images/member/04.jpg" alt="member-img">
-                                    </div>
-                                    <div class="lab-content">
-                                        <h6><a href="profile.html">Gyan-Baffour</a></h6>
-                                        <p>Active 5 Day</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-                            <div class="lab-item member-item style-1">
-                                <div class="lab-inner">
-                                    <div class="lab-thumb">
-                                        <img src="../public/images/member/05.jpg" alt="member-img">
-                                    </div>
-                                    <div class="lab-content">
-                                        <h6><a href="profile.html">Teszt Eleking</a></h6>
-                                        <p>Active 1 Day</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-                            <div class="lab-item member-item style-1">
-                                <div class="lab-inner">
-                                    <div class="lab-thumb">
-                                        <img src="../public//images/member/06.jpg" alt="member-img">
-                                    </div>
-                                    <div class="lab-content">
-                                        <h6><a href="profile.html">Zeahra Guido</a>
-                                        </h6>
-                                        <p>Active 1 Day</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <?php } ?>
+
                     </div>
 
                     <div class="member-button-group d-flex flex-wrap justify-content-center">
